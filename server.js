a651dca3d37eb521f385ee5d68fd46a804870755
MoisesContrerasC <moisescontrerascoronado@gmail.com>
2023-11-15 18:55:53 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,70 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const port = 3000;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+// Routers
+const todoRoutes = express.Router();
+const Todo = require('./model/todos.model');
 
-// Serve static files
-app.use(express.static(path.join(__dirname, '')));
 
-// Set up a route to serve the HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home.html'));
-});
+app.use(cors());
+app.use(bodyParser.json());
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+mongoose.connect('mongodb+srv://upatel:Z3BmV2ofUb7LbJw9@cluster0.n0akqrt.mongodb.net/?retryWrites=true&w=majority',
+{useNewUrlParser:true});
+
+const connection = mongoose.connection;
+
+connection.once('open',()=>{
+    console.log("DB connected......")
+})
+
+// YOU WRITE YOUR CODE HERE
+     todoRoutes.route('/').get((req, res)=>{
+      Todo.find()
+      .then(todos=>res.status(200).json(todos))
+        .catch(err=>res.status(400).json({"error": err}))
+     });
+
+     todoRoutes.route('/:id').get((req,res)=>{
+       Todo.findById(req.params.id)
+      .then(todos=>res.status(200).json(todos))
+        .catch(err=>res.status(400).json({"error": err}))
+     })
+
+     todoRoutes.route('/add').post((req,res)=>{
+        
+        let todo = new Todo(req.body)
+          todo.save()
+            .then(todos=>res.status(200).json(todos))
+            .catch(err=>res.status(400).json({"error": err}))
+
+     });
+
+     
+     todoRoutes.route('/update/:id').post((req,res)=>{
+        Todo.findById(req.params.id)
+        .then(todos=>{
+            //Update the object with new data
+            todos.description = req.body.description;
+            todos.responsible = req.body.responsible;
+            todos.priority = req.body.priority;
+            todos.isCompleted = req.body.isCompleted;
+
+            //Save new Data
+
+            todos.save()
+            .then(todos=>res.status(200).json(todos))
+            .catch(err=>res.status(400).json({"error": err}))
+
+            res.status(200).json(todos)
+        })
+          .catch(err=>res.status(400).json({"error": err}))
+       });
+
+app.use(todoRoutes);
+
+app.listen(8081,()=>{
+    console.log("Server is running on 8081....");
 });
