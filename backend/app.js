6709e41ee6ac923aa22129f4df58f5e7f151b9d1
Anand Sharma <ashar636@my.centennialcollege.ca>
2023-12-06 19:28:40 -0500

    Delete backend directory

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-
-
-const userRoutes = require('./routes/userRoute'); // Import the user routes
-
-app.use('/api/login', userRoutes); // Use the user routes
-module.exports = app;
--- backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { register, login } = require('../controllers/userControllers');
-
-
-router.post('/login', register);
-router.post('/login', login);
-
-module.exports = router;
