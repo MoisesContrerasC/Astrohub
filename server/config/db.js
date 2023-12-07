312265da41e3352c81b6294da5906e668b9379e5
Anand Sharma <ashar636@my.centennialcollege.ca>
2023-12-06 19:32:40 -0500

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //local MongoDB deployment ->
+    //"URI": "mongodb://localhost/mydb2"
+  "URI":"mongodb+srv://anandsksharma:1oEoAyp2N2WlCfVn@cluster0.pmqa9af.mongodb.net/?retryWrites=true&w=majority",
+  "Secret": 'SomeSecret'
+  };
+  
---
+++ server/routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+let indexController = require('../controllers/index');//
+const { route } = require('./users');
+
+/* GET home page. with login details */
+router.get('/',  indexController.displayHomePageWithLogin);
+
+/* GET home page. */
+
+router.get('/home',  indexController.displayHomePageWithLogin);
+
+
+/* GET billing page. with login details */
+router.get('/',  indexController.displayBillingPageWithLogin);
+
+/* GET billing page. */
+
+router.get('/billing',  indexController.displayBillingPageWithLogin);
+
+
+/* GET about us page. with login details */
+router.get('/',  indexController.displayAboutUsPageWithLogin);
+
+/* GET about us page. */
+
+router.get('/aboutus',  indexController.displayAboutUsPageWithLogin);
+
+/* GET Route for displaying the Login page */
+router.get('/login', indexController.displayLoginPage);
+
+/* POST Route for processing the Login page */
+router.post('/login', indexController.processLoginPage);
+
+
+
+/* GET Route for displaying the Register page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST Route for processing the Register page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET to perform UserLogout */
+router.get('/logout', indexController.performLogout);
+
+router.get('/surveyDB', function(req, res, next){
+  res.render('surveyDB', {title: 'Existing Survey Page'})
+})
+
+
+module.exports = router;
